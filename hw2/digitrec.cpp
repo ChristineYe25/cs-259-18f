9d00a126af126a48253c38b396a32571861d442d
yaying ye <yayingye@yayings-MacBook-Pro.local>
2018-10-25 14:41:12 -0700

    5

--- hw2/digitrec.cpp
+++ hw2/digitrec.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 const int memory_size=10;
 const int kBurstSize=180;
-const int kTileSize=18;
+const int kTileSize=10;
 
 void Load (const bool enable,unsigned long* data_dram, unsigned long* data_local){
 #pragma HLS inline off
