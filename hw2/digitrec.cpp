029d6e31fab83e8e142f802a599d84eb886a7426
yaying ye <yayingye@yayings-MacBook-Pro.local>
2018-10-25 13:51:57 -0700

    5

--- hw2/digitrec.cpp
+++ hw2/digitrec.cpp
@@ -14,7 +14,7 @@ void Load (const bool enable,unsigned long* data_dram, unsigned long* data_local
     if(enable){
 load:
     for(int i=0;i<kBurstSize;++i){
-#pragma HLS unroll
+#pragma HLS pipeline
         data_local[i]=data_dram[i];
     }
     }
@@ -29,9 +29,10 @@ void Compute(const bool enable,unsigned long* data_local, unsigned long test_ima
 #pragma HLS unroll
             data_local[i*kTileSize+j]=data_local[i*kTileSize+j]^test_image;
             unsigned long dis=0;
-            for(int z=0;z<49;++z){
+           /* for(int z=0;z<49;++z){
                 dis+=(data_local[i*kTileSize+j] & (1L<<z))>>z;
             }
+            */
             data_local[i*kTileSize+j]=dis;
             unsigned long max_id = 0;
             for (int k=0;k<3;++k){
