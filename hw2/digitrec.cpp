380339421eba70030e1f086b45b890de1b91aace
yaying ye <yayingye@yayings-MacBook-Pro.local>
2018-10-24 20:23:43 -0700

    bugs

--- hw2/digitrec.cpp
+++ hw2/digitrec.cpp
@@ -1,6 +1,8 @@
 #include <string.h>
 #include <math.h>
 #include <assert.h>
+#include <iostream>
+using namespace std;
 
 
 const int memory_size=100;
@@ -11,6 +13,8 @@ void Load (unsigned long* data_dram, unsigned long* data_local,int index){
 #pragma HLS unroll
         data_local[i]=data_dram[index+i];
     }
+    cout<<" load success";
+    
 }
 void Diff(unsigned long* data_local,unsigned long test_image){
 #pragma HLS inline off
@@ -18,6 +22,7 @@ void Diff(unsigned long* data_local,unsigned long test_image){
 #pragma HLS unroll
         data_local[i]=data_local[i]^test_image;
     }
+    cout<<" Diff success";
 }
 template<int n>
 void Reduce(unsigned long *array,int start){
@@ -26,6 +31,7 @@ void Reduce(unsigned long *array,int start){
 #pragma HLS unroll
         array[i]+=array[i+n];
     }
+    cout<<" reduce success";
 }
 void Dis(unsigned long* data_local){
 #pragma HLS inline off
@@ -36,6 +42,7 @@ void Dis(unsigned long* data_local){
             data_local[m]+=(data_local[m] & (1L<<i))>>i;
         }
     }
+    cout<<" dis success";
 /*
  #pragma HLS unroll
         for(int i=0;i<8;i++){
@@ -69,6 +76,7 @@ void Update (unsigned char* knn_mat,unsigned long* data_local,int x){
             knn_mat[max_id + (x*3)]=data_local[m];
          }
     }
+    cout<<" update success";
 }
 extern "C" {
 void digitrec_kernel(
@@ -95,7 +103,8 @@ init:
     }
 
  //the 10 digit loop
-/*    for(int i=0;i<10;i++){
+   for(int i=0;i<10;i++){
+       cout<<" test i =" + i;
 #pragma HLS pipeline
         for(int y=0;y<1800/memory_size;y++){
 #pragma HLS pipeline
@@ -106,7 +115,7 @@ init:
             
         }
     }
- */
+ 
 }
 
 } // extern "C"
