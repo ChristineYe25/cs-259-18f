30cae56c9b1bfb242ec47c5b842f7c94b17339c5
yaying ye <yayingye@yayings-MacBook-Pro.local>
2018-10-31 17:40:21 -0700

    part2

--- hw2-merlin/src/digitrec.cpp
+++ hw2-merlin/src/digitrec.cpp
@@ -8,7 +8,7 @@ void digitrec_kernel(
         unsigned long* train_images,
         unsigned char* knn_mat) {
 #pragma ACCEL interface variable=train_images depth=18000 bus_bitwidth=512
-#pragma ACCEL interface variable=knn_mat depth=30 bus_bitwidth=512
+#pragma ACCEL interface variable=knn_mat depth=30
     unsigned char buf_knn_mat[10][3];
 
     for (int x = 0; x < 10; ++x) {
@@ -19,7 +19,7 @@ void digitrec_kernel(
 
 
     for (int x = 0; x < 10; ++x) {
-#pragma ACCEL parallel factor=180
+#pragma ACCEL parallel factor=10
         for (int y = 0; y < 1800; ++y) {
             unsigned long temp = train_images[x * 1800 + y] ^ test_image;
             unsigned char dis = 0;
