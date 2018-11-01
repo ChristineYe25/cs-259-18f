b0c7c9fbd8b1bbf1534c7fc47fed81164bd415d8
yaying ye <yayingye@yayings-MacBook-Pro.local>
2018-11-01 16:49:34 -0700

    part2

--- hw2-merlin/src/digitrec.cpp
+++ hw2-merlin/src/digitrec.cpp
@@ -19,7 +19,7 @@ void digitrec_kernel(
 
 #pragma ACCEL parallel factor=10
     for (int x = 0; x < 10; ++x) {
-#pragma ACCEL tiling factor=256
+#pragma ACCEL tiling factor=200
         for (int y = 0; y < 1800; ++y) {
             unsigned long temp = train_images[x * 1800 + y] ^ test_image;
             unsigned char dis = 0;
