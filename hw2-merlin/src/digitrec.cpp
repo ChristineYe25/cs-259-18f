6fe42c8af05cda1115b8fbcb8f43101a7cf8c8a0
yaying ye <yayingye@yayings-MacBook-Pro.local>
2018-10-31 20:57:21 -0700

    part2

--- hw2-merlin/src/digitrec.cpp
+++ hw2-merlin/src/digitrec.cpp
@@ -18,9 +18,9 @@ void digitrec_kernel(
         }
     }
 
-#pragma ACCEL parallel
+#pragma ACCEL parallel factor=10
     for (int x = 0; x < 10; ++x) {
-#pragma ACCEL parallel
+#pragma ACCEL parallel factor=180
         for (int y = 0; y < 1800; ++y) {
             unsigned long temp = train_images[x * 1800 + y] ^ test_image;
             unsigned char dis = 0;
